6191f10cd2d483223d8fca6b56f4f93e4d6fad7a
Rajat Sharma <35336213+rotroapp@users.noreply.github.com>
2023-07-23 17:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function fibonacci(num) {
    let i=0;
+	let sum=0;
 	let j=1;
     while (num--) {
-		
+		sum= i+j;
 		i=j;
-    	j=j+i;
-		
+    	j=sum;
     }
-
-	return j;
+	return sum;
 }
 
 module.exports = fibonacci;
